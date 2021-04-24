7496ada341b47ce0f506ba64616e6e462a5becda
LordDarthos <rldmonroy@gmail.com>
2021-04-23 19:54:54 -0600

    commit

--- src/components/FormSignIn.js
+++ src/components/FormSignIn.js
@@ -50,7 +50,7 @@ const ImportForm = ({handleSubmit, handleUser, handleValue}) => {
               />
             </Form.Group>
 
-            <Button variant="primary" type="submit" >
+            <Button href="/" variant="primary" type="submit" >
                 Submit
             </Button>
             <Button className="mx-2" variant="secondary" type="button" >
